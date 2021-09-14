2ac4c8aac28569d9e55784edc13571b7e830432c
Aurélie <nn.aurelie@hotmail.com>
2021-09-14 13:24:25 +0200

    [Feature] Integration test for Restart.

--- src/tests/components/Game.test.js
+++ src/tests/components/Game.test.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Game from '../../components/Grid';
 
-describe('Game renders properly', () => {
+test('Game can be restarted', () => {
+  render(
+    <Game />,
+  );
 
+  // Make 1 play.
+  const coords = { x: 0, y: 0 };
+  const cellElem = document.querySelector(`[data-x='${coords.x}'][data-y='${coords.y}']`);
+  userEvent.click(cellElem);
+  expect(cellElem.textContent).toBeTruthy();
+
+  // Reset, and expect the grid to be clean again.
+  const resetBtn = screen.getByRole('button', { name: /restart/i });
+  userEvent.click(resetBtn);
+  expect(cellElem.textContent).toBeFalsy();
 });
