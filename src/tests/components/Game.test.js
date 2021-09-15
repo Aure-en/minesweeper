9a20a84f000a8d35bbc5a6d6ea541206e060e5f5
Aurélie <nn.aurelie@hotmail.com>
2021-09-15 22:27:20 +0200

    [Fix] Fixed bad import in <Game> test and dependency mistake.

--- src/tests/components/Game.test.js
+++ src/tests/components/Game.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Game from '../../components/Grid';
+import Game from '../../components/Game';
 
 test('Game can be restarted', () => {
   render(
