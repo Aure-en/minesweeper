07f5eb4b36cbca2240fe02909dc8b3a2129587a6
Aurélie <nn.aurelie@hotmail.com>
2021-09-22 15:10:12 +0200

    [Update] Grid is now always centered.

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -47,7 +47,9 @@ const Settings = ({ settings, setSettings }) => {
             background: themeContext.overlay,
           },
           content: {
+            width: '100%',
             maxWidth: '30rem',
+            minWidth: 'mit-content',
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
